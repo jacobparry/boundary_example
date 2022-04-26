259f24f278d6112c9065908697c6185fe96770e4
Jacob Parry <jacobparry@jacobs-mbp.lan>
2022-04-26 07:43:26 -0600

    added boundary config to check umbrella apps.

--- apps/api/lib/api.ex
+++ apps/api/lib/api.ex
@@ -1,5 +1,5 @@
 defmodule Api do
-  use Boundary, deps: [], exports: []
+  use Boundary, deps: [], exports: [], check: Application.get_env(:boundary, :umbrella_apps)
 
   def test do
     _user = %Db.Schemas.Example{}
--- apps/users/lib/users.ex
+++ apps/users/lib/users.ex
@@ -1,5 +1,5 @@
 defmodule Users do
-  use Boundary, deps: [], exports: [], check: Application.get_env(:boundary, :umbrella_apps)
+  use Boundary, deps: [Db], exports: [], check: Application.get_env(:boundary, :umbrella_apps)
 
   def hello do
     _user = %Db.Schemas.Example{}
